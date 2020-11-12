8bff981dad5aaf38fc8876deff46151842e48988
Mahmud-Alam <official.mahmud23@gmail.com>
2020-11-12 17:42:33 +0600

    fibonacci number dynamic programming way

---
+++ fibonacciNumberDynamicProgramming.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n){
+
+}
+
+int main(){
+    int n;
+    cout<<"Input number: ";cin>>n;
+
+    for(int i=0;i<n;i++){
+        cout<<fibonacci(i)<<"\t";
+    }
+    cout<<endl;
+}
