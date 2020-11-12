b34b709bb5540e6560345a509fdd1cb001ec9fe2
Mahmud-Alam <official.mahmud23@gmail.com>
2020-11-12 18:54:24 +0600

    sum of subset recursive way

--- sumOfSubsetRecursive.cpp
+++ sumOfSubsetRecursive.cpp
@@ -8,10 +8,16 @@ bool sumOfSubset(int sum, int n, int *arr){
 }
 int main(){
     int n, arr[100],sum;
-    cout<<"Input array size: ";cin>>n;
+    cout<<"Input array size: ";
+    cin>>n;
     cout<<endl<<"Input set of integers: "<<endl;
-    for(int i=0;i<n;i++)cin>>arr[i];
-    cout<<endl<<"Input Sum: ";cin>>sum;
-    if (sumOfSubset(sum,n,arr))cout<<endl<<"Yes"<<endl;
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+
+    cout<<endl<<"Input Sum: ";
+    cin>>sum;
+
+    if (sumOfSubset(sum,n,arr))
+        cout<<endl<<"Yes"<<endl;
     else cout<<endl<<"No"<<endl;
 }
