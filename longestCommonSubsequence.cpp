9c4006f4b8b1c8cc8be30d597ed261779750f28a
Mahmud-Alam <official.mahmud23@gmail.com>
2020-11-19 21:10:30 +0600

    LCS problem

---
+++ longestCommonSubsequence.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 20
+
+int lookup[MAX][MAX];
+
+string LCS(string X, string Y, int m, int n){
+	if (m == 0 || n == 0)
+		return string("");
+
+	if (X[m - 1] == Y[n - 1]){
+		return LCS(X, Y, m - 1, n - 1) + X[m - 1];
+	}
+
+	if (lookup[m - 1][n] > lookup[m][n - 1])
+		return LCS(X, Y, m - 1, n);
+
+	else
+		return LCS(X, Y, m, n - 1);
+}
+
+void LCSLength(string X, string Y, int m, int n){
+	for (int i = 1; i <= m; i++){
+		for (int j = 1; j <= n; j++){
+			if (X[i - 1] == Y[j - 1])
+				lookup[i][j] = lookup[i - 1][j - 1] + 1;
+			else
+				lookup[i][j] = max(lookup[i - 1][j], lookup[i][j - 1]);
+		}
+	}
+}
+
+int main(){
+	string X = "BCDAACD", Y = "ACDBAC";
+	int m = X.length(), n = Y.length();
+
+	LCSLength(X, Y, m, n);
+
+	cout<<"The LCS length is: "<<LCS(X, Y, m, n).length()<<endl<<endl<<"The LCS String is: "<<LCS(X, Y, m, n)<<endl;
+}
