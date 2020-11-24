fae27e4151797343ce6b4f9f2f642e1c6c65ab89
Mahmud-Alam <official.mahmud23@gmail.com>
2020-11-24 18:43:56 +0600

    0-1 knapsack own dp

---
+++ 0-1KnapsackOwnDp.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int knapsack(int w, int n, int *wt, int *val){
+    int p[n+1][w+1];
+
+    for(int i=0;i<=n;i++)
+        p[i][0] = 0;
+    for(int i=0;i<=w;i++)
+        p[0][i] = 0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=w;j++){
+            if(j<wt[i-1]){
+                p[i][j] = p[i-1][j];
+            }
+            if(j>=wt[i-1]){
+                p[i][j] = max(p[i-1][j], val[i-1]+p[i-1][j-wt[i-1]]);
+            }
+        }
+    }
+    return p[n][w];
+}
+
+int main(){
+    int w;
+    int wt[] = {2,1,3,2}, val[] = {12,10,20,15};
+    int n = sizeof(wt)/sizeof(wt[0]);
+
+    cout<<"Input a Knapsack Weight: ";cin>>w;
+
+    cout<<endl<<"Total value: "<<knapsack(w,n,wt,val)<<endl;
+}
