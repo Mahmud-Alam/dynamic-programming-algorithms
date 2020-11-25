e57173804df7bd8d696486add3c550b6588b1921
Mahmud-Alam <official.mahmud23@gmail.com>
2020-11-25 11:34:37 +0600

    0-1 knapsack own dp with items

--- 0-1KnapsackWithItemsOwnDp.cpp
+++ 0-1KnapsackWithItemsOwnDp.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int knapsack(int w, int n, int *wt, int *val){
     int p[n+1][w+1];
-    //int sack[n];
+    int sack[n][w];
 
     for(int i=0;i<=n;i++)
         p[i][0] = 0;
@@ -13,22 +13,58 @@ int knapsack(int w, int n, int *wt, int *val){
         for(int j=1;j<=w;j++){
             if(j<wt[i-1]){
                 p[i][j] = p[i-1][j];
+                sack[i-1][j-1] = 0;
             }
             if(j>=wt[i-1]){
-                if(p[i-1][j]> val[i-1] + p[i-1][j-wt[i-1]])
+                if(p[i-1][j]> val[i-1] + p[i-1][j-wt[i-1]]){
                     p[i][j] = p[i-1][j];
-                else p[i][j] = val[i-1] + p[i-1][j-wt[i-1]];
+                    sack[i-1][j-1] = 0;
+                }
+                else{
+                    p[i][j] = val[i-1] + p[i-1][j-wt[i-1]];
+                    sack[i-1][j-1] = 1;
+                }
             }
         }
     }
 
-     cout<<endl;
+
+    //Knapsack Table print
+    cout<<endl;
     for(int i=0;i<=n;i++){
         for(int j=0;j<=w;j++){
             cout<<p[i][j]<<"\t";
         }
         cout<<endl;
     }
+
+
+
+    //Selected items print
+    int res = p[n][w];
+
+    int W = w;
+    cout<<endl<<endl<<"Selected items are: "<<endl<<endl;
+    for (int i=n; i>0&&res>0; i--) {
+
+        // either the result comes from the top
+        // (K[i-1][w]) or from (val[i-1] + K[i-1]
+        // [w-wt[i-1]]) as in Knapsack table. If
+        // it comes from the latter one/ it means
+        // the item is included.
+        if(res == p[i-1][W])
+            continue;
+        else{
+            // This item is included.
+            cout<<i<<"item\t weight: "<<wt[i - 1]<<endl;
+
+            // Since this weight is included its
+            // value is deducted
+            res = res - val[i-1];
+            W = W - wt[i-1];
+        }
+    }
+    cout<<endl;
 
     return p[n][w];
 }
